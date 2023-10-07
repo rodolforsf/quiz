8edf4f214c098aa47d6b350b9dc6f1414236d94f
Rodolfo Faria <106396728+rodolforsf@users.noreply.github.com>
2023-10-07 10:26:00 -0300

    Primeira pergunta criada

---
+++ src/components/data/questions.ts
@@ -0,0 +1,14 @@
+import { Question } from "../types/Question";
+
+export const questions: Question[] = [
+    {
+        question: 'Em que ano o Brasil se tornou tri-campeão da Copa do Mundo?',
+        options: [
+            '1962',
+            '1996',
+            '1970',
+            '1940'
+        ],
+        answer: 2
+    }
+];
