5dde43fc4cfae578fcfbb736db3abcf42cbe589a
Rodolfo Faria <106396728+rodolforsf@users.noreply.github.com>
2023-10-07 13:20:22 -0300

    Mostrando no resultado as respostas certas e se acertou

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -1,7 +1,22 @@
-export const Results = ()=> {
+import { Question } from "./types/Question";
+
+type Props = {
+    questions: Question[];
+    answers: number[];
+}
+
+export const Results = ({ questions, answers}: Props)=> {
     return (
         <div className="p-1">
-            123
+            {questions.map((item, key) => (
+                <div key={key} className="mb-3">
+                    <div className="fon-bold">{key + 1}. {item.question}</div>
+                    <div>
+                        <span>({item.answer === answers[key] ? 'Acertou' : 'Errou'}) - </span>
+                        {item.options[item.answer]}
+                    </div>
+                </div>
+            ))}
         </div>
     );
 }
